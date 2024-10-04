3eda9cad5e66abd6065ffc932baa5ab125e3afe1
云起时 <1227233368@qq.com>
2024-10-04 17:22:57 +0800

    修改了文件结构

---
+++ Assets/Scripts/InGame/CanvasBehavior.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using Newtonsoft.Json;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CanvasBehavior : MonoBehaviour
+{
+    public GameObject nodePrefab;
+    public GameObject connectionPrefab;
+    private Camera cam;
+    private Plane plane; // 用于定义水平面
+    [SerializeField]
+    private List<GameObject> nodeList;
+    [SerializeField]
+    private List<GameObject> connectionList;
+
+
+    
+    [System.Serializable]
+    public class NodeData
+    {
+        //node
+        public int id;
+        public float[] position;
+        public Properties properties;
+        public NodeData(int id, float[] position, Properties properties = null)
+        {
+            this.id = id;
+            this.position = new float[] { position[0], position[1], position[2] };
+            this.properties = properties ?? new Properties
+            {
+                awakeThreshold = 0, // default
+                exposeThreshold = 0, // default
+                maximumBooks = 0 // default
+            };
+        }
+
+        public Vector3 GetVector3Position()
+        {
+            return new Vector3(position[0], position[1], position[2]);
+        }
+    }
+
+    [System.Serializable]
+    public class ConnectionData
+    {
+        //connection
+        public int startNodeId;
+        public int endNodeId;
+
+        public ConnectionData(int startId, int endId)
+        {
+            startNodeId = startId;
+            endNodeId = endId;
+        }
+    }
+
+    [System.Serializable]
+    public class ConfigurationData
+    {
+        //读取时使用
+        public List<NodeData> positions;
+        public List<ConnectionData> connections;
+    }
+
+    public void LoadConfiguration(string filePath)
+    {
+        if (System.IO.File.Exists(filePath))
+        {
+            string json = System.IO.File.ReadAllText(filePath);
+            ConfigurationData configData = JsonConvert.DeserializeObject<ConfigurationData>(json);
+
+            // 实例化节点
+            foreach (NodeData position in configData.positions)
+            {
+                GameObject node = Instantiate(nodePrefab, position.GetVector3Position(), Quaternion.identity, transform);
+                node.name = $"Node_{position.id}"; // 为节点命名
+                NodeBehavior nodeBehavior = node.GetComponent<NodeBehavior>();
+                if (nodeBehavior != null)
+                {
+                    Properties loadedProperties = position.properties ?? new Properties();
+
+                    // 如果字段为默认值，则填充默认值
+                    nodeBehavior.properties = new Properties
+                    {
+                        awakeThreshold = loadedProperties.awakeThreshold != 0 ? loadedProperties.awakeThreshold : 0,
+                        exposeThreshold = loadedProperties.exposeThreshold != 0 ? loadedProperties.exposeThreshold : 0,
+                        maximumBooks = loadedProperties.maximumBooks != 0 ? loadedProperties.maximumBooks : 0
+                    };
+                }
+                nodeList.Add(node);
+            }
+
+            // 实例化连接
+            foreach (ConnectionData connection in configData.connections)
+            {
+                if (connection.startNodeId < nodeList.Count && connection.endNodeId < nodeList.Count)
+                {
+                    GameObject connectionObj = Instantiate(connectionPrefab, transform);
+                    ConnectionBehavior connectionScript = connectionObj.GetComponent<ConnectionBehavior>();
+
+                    if (connectionScript != null)
+                    {
+                        connectionScript.startNode = nodeList[connection.startNodeId];
+                        connectionScript.endNode = nodeList[connection.endNodeId];
+                    }
+                    connectionList.Add(connectionObj);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError($"File not found: {filePath}");
+        }
+    }
+
+    void Start()
+    {
+        cam = Camera.main;
+        plane = new Plane(Vector3.up, Vector3.zero);
+        nodeList = new List<GameObject>();
+        connectionList = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public List<GameObject> GetNeighbors(GameObject node)
+    {
+        List<GameObject> neighbors = new List<GameObject>();
+
+        //TODO
+
+        return neighbors;
+    }
+
+    public void RefreshAllNodes()
+    {
+        //TODO
+    }
+    public void ResetAllNodes()
+    {
+        //TODO
+    }
+    public void CreateNode()
+    {
+        //create a node in the center of the screen
+        //不删，没准以后什么拓展功能有用
+        Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        float enter;
+        Vector3 point = Vector3.zero;
+        if (plane.Raycast(ray, out enter))
+        {
+            point = ray.GetPoint(enter);
+        }
+        GameObject newNode = Instantiate(nodePrefab, point, Quaternion.Euler(Vector3.zero), gameObject.transform);
+        newNode.name = $"Node";
+        nodeList.Add(newNode);
+
+    }
+}
---
+++ Assets/Scripts/InGame/NodeBehavior.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NodeBehavior : MonoBehaviour
+{
+    public Properties properties;
+    private Renderer objRenderer;
+    private Dictionary<int, Color> ColorMap;
+    public bool selected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objRenderer = GetComponent<MeshRenderer>();
+
+        objRenderer.material = new Material(objRenderer.material);
+
+        ColorMap = new Dictionary<int, Color>();
+        ColorMap.Add(-1, Color.gray);
+        ColorMap.Add(0, Color.green);
+        ColorMap.Add(1, Color.yellow);
+        ColorMap.Add(2, Color.red);
+    }
+
+    //public void OnObjectClicked()
+    //{
+    //    if (properties.state != Properties.StateEnum.EXPOSED) properties.state++;
+    //    else properties.state = Properties.StateEnum.DEAD;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        objRenderer.material.color = selected ? Color.red : Color.white;
+    }
+
+    //public Properties.StateEnum RefreshState()
+    //{
+    /*
+    CanvasEditorBehavior cb = transform.parent.GetComponent<CanvasEditorBehavior>();
+    if(cb == null)
+    {
+        Debug.LogWarning("Script \"CanvasBehavior\" not found in canvas.");
+        return Properties.StateEnum.DEAD;
+    }
+    List<GameObject> nList = cb.GetNeighbors(gameObject);
+    int Influence = 0;
+    foreach(GameObject go in nList)
+    {
+        CubeEditorBehavior cub = go.GetComponent<CubeEditorBehavior>();
+        if (cub != null) Influence += cub.properties.state > 0 ? cub.properties.influence : 0;
+        if (cub != null) Influence -= cub.properties.state < 0 ? 2 * cub.properties.influence : 0;
+    }
+    //Debug.Log(2);
+    if (properties.state == Properties.StateEnum.DEAD) return Properties.StateEnum.DEAD;
+    else if (Influence >= properties.exposeThreshold) return Properties.StateEnum.EXPOSED;
+    else if (properties.state == Properties.StateEnum.NORMAL && Influence >= properties.awakeThreshold) return Properties.StateEnum.AWAKENED;
+    else if (properties.state >= Properties.StateEnum.AWAKENED && Influence <= properties.supressThreshold) return Properties.StateEnum.NORMAL; 
+    else return (int)properties.state > 1 ? properties.state - 1 : properties.state;
+    */
+    //}
+    //public void SetState(Properties.StateEnum stateEnum)
+    //{
+    //    properties.state = stateEnum;
+    //}
+}
