82061ee003acae51ea03f8bb7b72b246aa37fce8
云起时 <1227233368@qq.com>
2024-10-27 12:29:45 +0800

    完善了PlotManager 当前可以在Debug中预览剧情，但由于没有事件回馈，交互逻辑略有差异。 包括但不限于无法退出剧情。

--- Assets/Scripts/InGame/Manager/PlotManager.cs
+++ Assets/Scripts/InGame/Manager/PlotManager.cs
@@ -12,13 +12,23 @@ public class PlotManager : MonoBehaviour
 
     public delegate void UserActionEventHandler(bool forSelect,int selectIndex);
     public event UserActionEventHandler UserAction;
+    public void TriggerUserAction(bool forSelect,int selectIndex)
+    {
+        UserAction?.Invoke(forSelect,selectIndex);
+    }
+
     public event Action PlotStart;
+    public void TriggerPlotStart()
+    {
+        PlotStart?.Invoke();
+    }
     int next;
     Dictionary<int, string> nameMap;
     Dictionary<string, int> flagMap;
     List<string> choices;
     List<string> selectionFlags;
     string[] lines;
+    bool duringPlot;
 
 
     private void Awake()
@@ -36,10 +46,17 @@ public class PlotManager : MonoBehaviour
         nameMap = new Dictionary<int, string>();
         choices = new List<string>();
         selectionFlags = new List<string>();
+        duringPlot = false;
     }
 
     public void StartPlot(string filePath)
     {
+        if (duringPlot)
+        {
+            Debug.LogWarning("不可重复触发剧情。");
+            return;
+        }
+        duringPlot = true;
         next = 0;
         flagMap.Clear();
         nameMap.Clear();
@@ -76,7 +93,7 @@ public class PlotManager : MonoBehaviour
     }
     private void OnEnd()
     {
-        //?
+        duringPlot = false;
     }
 
     private void OnNextStep(bool forSelect, int selectIndex)
@@ -99,9 +116,8 @@ public class PlotManager : MonoBehaviour
             // 获取当前行并去除空格
             string line = lines[next].Trim();
 
-            if (line.StartsWith("say ") || line.StartsWith("Isay "))
+            if (line.StartsWith("say "))
             {
-                bool isMe = line.StartsWith("Isay ") ? true : false;
 
                 StringBuilder commandBuilder = new StringBuilder(line);
 
@@ -121,8 +137,32 @@ public class PlotManager : MonoBehaviour
                     dialogContent = dialogContent.Replace("\\n", "\n");
 
                     // 推送对话
-                    if (isMe) PushSelfDialog(nameMap[characterId], dialogContent);
-                    else PushDialog(nameMap[characterId], dialogContent);
+                    PushDialog(nameMap[characterId], dialogContent);
+                    break; // 中断循环，等待用户操作后再调用 NextStep
+                }
+            }
+            else if (line.StartsWith("Isay "))
+            {
+
+                StringBuilder commandBuilder = new StringBuilder(line);
+
+                while (commandBuilder.ToString().EndsWith("\\"))
+                {
+                    commandBuilder.Length--;
+                    next++;
+                    commandBuilder.Append(lines[next].Trim());
+                }
+
+                string fullCommand = commandBuilder.ToString();
+
+                int firstSpaceIndex = fullCommand.IndexOf(' ', 5);
+                if (firstSpaceIndex != -1 && int.TryParse(fullCommand.Substring(5, firstSpaceIndex - 5), out int characterId))
+                {
+                    string dialogContent = fullCommand.Substring(firstSpaceIndex + 1).Trim('\'');
+                    dialogContent = dialogContent.Replace("\\n", "\n");
+
+                    // 推送对话
+                    PushSelfDialog(nameMap[characterId], dialogContent);
                     break; // 中断循环，等待用户操作后再调用 NextStep
                 }
             }
@@ -194,10 +234,16 @@ public class PlotManager : MonoBehaviour
     }
 
     private void PushSelection(List<string> selectItems)
-    { 
+    {
         //TODO
         //这里向ui推送一个选择，ui检测到用户进行“选择”动作后应当触发此类内事件UserAction
         //置其首参数为true表示当前需要响应select，第二个参数填入响应时跳转到的flag
+        StringBuilder s = new StringBuilder("请做出选择：\n");
+        for(int i=0;i<selectItems.Count;i++)
+        {
+            s.Append($"{i + 1}. {selectItems[i]}\n");
+        }
+        Debug.Log(s.ToString());
     }
 
     private int GetFlagLineNumber(string flagName)
---
+++ Assets/Scripts/InGame/UI/PlotUI/PlotDisplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotDisplay : MonoBehaviour
+{
+    public void NextStep()
+    {
+        PlotManager.Instance.TriggerUserAction(false, 0);
+    }
+
+    public void Select()
+    {
+        PlotManager.Instance.TriggerUserAction(true, 0);
+    }
+}
