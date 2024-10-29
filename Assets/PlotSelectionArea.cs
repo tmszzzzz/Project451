3da928e377dc68c33f8b9b41275e4a1cf2a37a1b
Cor <1035711820@qq.com>
2024-10-29 22:37:23 +0800

    显示有一个小bug

---
+++ Assets/PlotSelectionArea.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlotSelectionArea : MonoBehaviour
+{
+    //Use three buttons here
+    [SerializeField] private List<GameObject> allButtons;
+
+    public void ClearCurrentButtons()
+    {
+        foreach(GameObject button in allButtons) 
+        {
+            button.GetComponent<Button>().onClick.RemoveAllListeners();
+            button.SetActive(false);
+        }
+    }
+
+    public List<Button> NeedButtons(int buttonNum)
+    {
+        List<Button> neededButtons = new List<Button>();
+
+        for (int i = 0; i < buttonNum && i < allButtons.Count; i++)
+        {
+            //Debug.Log("set active");
+            //Debug.Log(allButtons[i].activeSelf);
+            allButtons[i].GetComponent<Button>().onClick.AddListener(ClearCurrentButtons);
+            //Debug.Log(allButtons[i].activeSelf);
+            neededButtons.Add(allButtons[i].GetComponent<Button>());
+            allButtons[i].SetActive(true);
+        }
+
+        return neededButtons;
+    }
+
+    public void ClosePlots()
+    {
+        foreach(GameObject button in allButtons)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject button in allButtons)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/InGame/UI/2dUI/PlotUI/PlotDisplay.cs
+++ Assets/Scripts/InGame/UI/2dUI/PlotUI/PlotDisplay.cs
@@ -1,40 +1,74 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.UI;
 
 public class PlotDisplay : MonoBehaviour
 {
-    // 引用 TextMeshProUGUI 组件
-    [SerializeField] private TextMeshProUGUI plotText;
-
-    // 引用继续按钮和选择项按钮的父容器
-    [SerializeField] private GameObject continueButton;
-    [SerializeField] private Transform selectionContainer;
-    [SerializeField] private Button selectionButtonPrefab;
-
-    private void Awake()
+    [SerializeField] private GameObject plotPanel;
+    [SerializeField] private GameObject plotDisplayArea;
+    [SerializeField] private GameObject plotSelectionArea;
+    [SerializeField] private Image shadowImage;
+    [SerializeField] private float shadowAlpha = 0.95f; 
+    [SerializeField] private List<Image> imagesTobeFaded;
+    private bool isPlotting = false;
+    [SerializeField] private float lerpAlphaSpeed = 10f;
+    void Update()
     {
-        continueButton.GetComponent<Button>().onClick.AddListener(ContinuePlot);
-        plotText.gameObject.SetActive(false);
+        if(shadowImage.color.a < 0.1f && isPlotting == false)
+        {
+            shadowImage.gameObject.SetActive(false);
+            plotPanel.SetActive(false);
+        }
+
+        float targetShadowAlpha = isPlotting ? shadowAlpha : 0f;
+        shadowImage.color = new Color(shadowImage.color.r, shadowImage.color.g, shadowImage.color.b, Mathf.Lerp(shadowImage.color.a, targetShadowAlpha, Time.deltaTime * lerpAlphaSpeed));
+
+        float targetAlpha = isPlotting ? 1f : 0f;
+        foreach (Image image in imagesTobeFaded)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Lerp(image.color.a, targetAlpha, Time.deltaTime * lerpAlphaSpeed));
+        }
     }
 
+    void OpenPlots()
+    {
+        if (isPlotting)
+        {
+            Debug.Log("Plot again");
+        }
+        shadowImage.gameObject.SetActive(true);
+        plotPanel.SetActive(true);
+        isPlotting = true;
+    }
+
+
+    void ClosePlots()
+    {
+        if (!isPlotting)
+        {
+            Debug.Log("Plot is not open already");
+        }
+        isPlotting = false;
+        plotDisplayArea.GetComponent<PlotDisplayArea>().ClosePlots();
+        plotSelectionArea.GetComponent<PlotSelectionArea>().ClosePlots(); 
+    }
 
     // 开始新剧情时调用
     public void PushedStart()
     {
-        plotText.gameObject.SetActive(true);
-        plotText.text = ""; // 清空之前的剧情文本
+        OpenPlots();
         PlotManager.Instance.TriggerPlotStart();
+        CreateContinueBUtton();
     }
+
+
 
     // 结束剧情时调用
     public void PushedEnd()
     {
-
-        plotText.text += "\n\n---END---"; // 显示剧情结束标记
-        continueButton.SetActive(false); // 隐藏继续按钮
-        selectionContainer.gameObject.SetActive(false); // 隐藏选择项容器
+        ClosePlots();
         PlotManager.Instance.TriggerPlotEnd();
     }
 
@@ -45,60 +79,68 @@ public class PlotDisplay : MonoBehaviour
         {
             Debug.LogWarning("当前不在剧情中，点击此按钮无效。");
         }
-        // 隐藏继续按钮，并向 PlotManager 发送继续剧情的信号
-        continueButton.SetActive(false);
+
         PlotManager.Instance.TriggerUserAction(false, 0);
+    }
+
+    void CreateContinueBUtton()
+    {
+        Debug.Log("calling from selection area");
+        Button continueButton = plotSelectionArea.GetComponent<PlotSelectionArea>().NeedButtons(1)[0];
+        //Debug.Log(continueButton.gameObject.activeSelf);
+        continueButton.onClick.AddListener(ContinuePlot);
     }
 
     // 显示对侧对话
     public void ShowDialog(string name, string content)
     {
-        plotText.text += $"{name}:\n{content}\n\n";
-        continueButton.SetActive(true);
+        plotDisplayArea.GetComponent<PlotDisplayArea>().PlotNewText(false, name, content);
+        CreateContinueBUtton();
     }
 
     // 显示自身对话
     public void ShowSelfDialog(string name, string content)
     {
-        plotText.text += $"{name} (Me):\n{content}\n\n";
-        continueButton.SetActive(true);
+        plotDisplayArea.GetComponent<PlotDisplayArea>().PlotNewText(true, name, content);
+        CreateContinueBUtton();
     }
 
     // 显示旁白
     public void ShowNarration(string content)
     {
-        plotText.text += $"旁白:\n{content}\n\n";
-        continueButton.SetActive(true);
+        Debug.Log("Already abandon this function");
+        // plotText.text += $"旁白:\n{content}\n\n";
+        // continueButton.SetActive(true);
     }
 
     // 显示选择项
     public void ShowSelection(List<string> choices)
     {
-        selectionContainer.gameObject.SetActive(true);
+        // selectionContainer.gameObject.SetActive(true);
 
-        // 清空之前的选择项
-        foreach (Transform child in selectionContainer)
-        {
-            Destroy(child.gameObject);
-        }
+        // // 清空之前的选择项
+        // foreach (Transform child in selectionContainer)
+        // {
+        //     Destroy(child.gameObject);
+        // }
 
-        // 创建新的选择项
-        for (int i = 0; i < choices.Count; i++)
-        {
-            string choiceText = choices[i];
-            int choiceIndex = i;
+        // // 创建新的选择项
+        // for (int i = 0; i < choices.Count; i++)
+        // {
+        //     string choiceText = choices[i];
+        //     int choiceIndex = i;
 
-            Button choiceButton = Instantiate(selectionButtonPrefab, selectionContainer);
-            choiceButton.GetComponentInChildren<TextMeshProUGUI>().text = choiceText;
-            choiceButton.onClick.AddListener(() => SelectChoice(choiceIndex));
-        }
+        //     Button choiceButton = Instantiate(selectionButtonPrefab, selectionContainer);
+        //     choiceButton.GetComponentInChildren<TextMeshProUGUI>().text = choiceText;
+        //     choiceButton.onClick.AddListener(() => SelectChoice(choiceIndex));
+        // }
     }
 
     // 选择一个选项
     private void SelectChoice(int choiceIndex)
     {
-        selectionContainer.gameObject.SetActive(false); // 隐藏选择项
-        PlotManager.Instance.TriggerUserAction(true, choiceIndex); // 通知 PlotManager 选择了某项
+        // selectionContainer.gameObject.SetActive(false); // 隐藏选择项
+        // PlotManager.Instance.TriggerUserAction(true, choiceIndex); // 通知 PlotManager 选择了某项
     }
 
 }
