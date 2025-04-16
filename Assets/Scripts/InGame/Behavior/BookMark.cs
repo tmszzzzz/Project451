a79c4b1046287c07adbec86c9be1fcabc2d621b4
云起时 <1227233368@qq.com>
2025-04-16 18:06:30 +0800

    书签视觉效果强化

--- Assets/Scripts/InGame/Behavior/BookMark.cs
+++ Assets/Scripts/InGame/Behavior/BookMark.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BookMark : MonoBehaviour
 {
-    [System.Serializable]
-    public class InfluenceVisualConfig
-    {
-        public Sprite patternSprite;  // 花纹贴图
-    }
     
     [System.Serializable]
     public class TypeConfig
     {
         public BookManager.Book.BookType type;
-        public InfluenceVisualConfig[] influenceConfigs;
+        public List<Material> influenceConfigs;
     }
     
     [SerializeField] private Image patternImage;
     [SerializeField] private TextMeshProUGUI bookNameText;
     
-    [SerializeField] private TypeConfig[] typeConfigurations;
+    [SerializeField] private List<TypeConfig> typeConfigurations;
     public int nodeIndex;
     public BookManager.Book book;
     // 配置书签视觉和交互
@@ -29,14 +24,21 @@ public class BookMark : MonoBehaviour
     {
         book = associatedBook;
         nodeIndex = index;
+        bool find = false;
         foreach (var v in typeConfigurations)
         {
+            
             if (book.type == v.type)
             {
-                patternImage.sprite = v.influenceConfigs[book.basicInfluence - 1].patternSprite;
+                find = true;
+                if (book.basicInfluence > 3)
+                {
+                    Debug.LogWarning($"Book {book.name} has illegal influence: {book.basicInfluence}");
+                }
+                patternImage.material = v.influenceConfigs[book.basicInfluence - 1];
                 break;
             }
-        }
+        }if(!find) Debug.LogWarning($"No match type for {book.id}-{book.name}: {book.type}.");
         bookNameText.text = book.name;
     }
     
