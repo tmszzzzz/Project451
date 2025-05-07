cd9bc7b6c3ae456440c6b7e42d9eb461036d90ee
snowsky19 <2206602919@qq.com>
2025-05-07 15:14:54 +0800

    ImagePlay更新

--- Assets/Scripts/InGame/ImagePlay.cs
+++ Assets/Scripts/InGame/ImagePlay.cs
@@ -26,6 +26,7 @@ public class ImagePlay : MonoBehaviour
         else
         {
             gameObject.SetActive(false);
+            ImagePlayController.instance.PlayNextPage();
         }
     }
     
---
+++ Assets/Scripts/InGame/ImagePlayController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagePlayController : MonoBehaviour
+{
+    public static ImagePlayController instance;
+    public List<ImagePlay> imagePlayPages;
+    private int currentPage = 0;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    void Start()
+    {
+        PlayNextPage();
+    }
+    
+    public void PlayNextPage()
+    {
+        if (currentPage < imagePlayPages.Count && currentPage > -1)
+        {
+            imagePlayPages[currentPage++].gameObject.SetActive(true);
+        }
+    }
+}
